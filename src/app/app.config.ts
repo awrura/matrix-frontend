521dbb3b78ae02f19f4d337485d1f60340095636
Алексей Смыков <asmykov@embedika.ru>
2024-08-07 18:03:49 +0700

    build: базовые настрйоки проекта

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAngularSvgIcon(),
+    provideHttpClient(),
+  ],
 };
