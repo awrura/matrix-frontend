fc23faed8020c0c8e5aa707a9da0b6a469176731
Алексей Смыков <asmykov@embedika.ru>
2024-08-07 18:05:31 +0700

    feat: страница прикручена к проекту

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatrixEditorComponent } from './components/matrix-editor/matrix-editor.component';
+import { SvgIconRegistryService } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterOutlet, MatrixEditorComponent],
+  providers: [SvgIconRegistryService],
 })
-export class AppComponent {
-  title = 'matrix-editor';
-}
+export class AppComponent {}
